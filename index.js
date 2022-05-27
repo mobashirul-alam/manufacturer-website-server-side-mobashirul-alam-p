d8836fdb619c5316128ac4dee30eff9f11fdfa96
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-27 21:24:26 +0600

    api for getting specific tool

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -24,7 +24,14 @@ async function run() {
             const cursor = toolsCollection.find(query);
             const results = await cursor.toArray();
             res.send(results);
-        })
+        });
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.findOne(query);
+            res.send(result);
+        });
     } finally {
 
     }
