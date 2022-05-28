2b9ace9c3ec35274c9118f29ecfaa7ecfa22aa8d
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-28 20:50:21 +0600

    verify jwt added and payment processing

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 app.use(cors());
 app.use(express.json())
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zu0ff.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
 
 async function run() {
     try {
@@ -22,6 +39,18 @@ async function run() {
         const reviewsCollection = client.db('goldenWeightTools').collection('reviews');
         const usersCollection = client.db('goldenWeightTools').collection('users');
         const usersProfileCollection = client.db('goldenWeightTools').collection('userProfile');
+
+        app.post('/create-payment-intent', verifyJwt, async (req, res) => {
+            const order = req.body;
+            const price = order.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -43,12 +72,24 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/order/:email', async (req, res) => {
+        app.get('/order/:email', verifyJwt, async (req, res) => {
             const email = req.params.email;
-            const query = { email: email };
-            const result = await ordersCollection.find(query).toArray();
-            res.send(result);
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const result = await ordersCollection.find(query).toArray();
+                return res.send(result);
+            } else {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
         });
+
+        app.get('/orders/:id', verifyJwt, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.findOne(query);
+            res.send(result);
+        })
 
         app.post('/reviews', async (req, res) => {
             const review = req.body;
@@ -71,7 +112,8 @@ async function run() {
                 $set: user
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token });
         });
 
         app.put('/usersProfile/:email', async (req, res) => {
